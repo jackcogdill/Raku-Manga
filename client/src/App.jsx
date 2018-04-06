9207a2430127ef39514e8337c63b7b02e9a9190c
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-04-06 14:53:36 -0400

    Add linting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
 import logo from './logo.svg';
-
 import './App.css';
 
 class App extends Component {
-    state = {
-        response: ''
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            response: '',
+        };
+    }
 
     componentDidMount() {
         this.callApi()
@@ -27,11 +28,11 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-            <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            </header>
-            <p className="App-intro">{this.state.response}</p>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">{this.state.response}</p>
             </div>
         );
     }
