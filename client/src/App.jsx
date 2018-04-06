5185d4576088b4a70594d520b65dcd6a61150b33
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-04-06 16:12:59 -0400

    Basic data transfer of sorted filenames

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,12 +12,12 @@ class App extends Component {
 
     componentDidMount() {
         this.callApi()
-            .then(res => this.setState({ response: res.express }))
+            .then(res => this.setState({ response: res.data }))
             .catch(err => console.log(err));
     }
 
     callApi = async () => {
-        const response = await fetch('/api/hello');
+        const response = await fetch('/api/manga');
         const body = await response.json();
 
         if (response.status !== 200) throw Error(body.message);
--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const glob = require('glob');
+const argv = require('minimist')(process.argv.slice(2));
+const sort = require('./sort');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-    res.send({ express: 'Hello From Express' });
+const formats = ['jpg', 'jpeg', 'gif', 'png', 'tiff', 'bmp'];
+const pattern = `${argv.dir}/*@(${formats.join('|')})`;
+
+console.dir(argv);
+
+app.get('/api/manga', (req, res) => {
+    glob(pattern, {}, (err, files) => { // Options is optional
+        files.sort(sort.numCompare);
+        res.send({
+            data: JSON.stringify(files, null, 4),
+        });
+    });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
