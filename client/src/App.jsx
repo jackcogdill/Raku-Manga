f50b7382e96b2f6777c5b6a9bcf388daf6daa272
Jack Cogdill <jackcogdill@users.noreply.github.com>
2018-05-05 00:20:49 -0400

    Set up code to use cookies. Print scrollTop.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,11 @@
 import React, { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 import InfiniteScroll from 'react-infinite-scroller';
+import Cookies from 'universal-cookie';
 
 import './App.css';
+
+const cookies = new Cookies();
 
 class App extends Component {
     constructor(props) {
@@ -15,10 +18,32 @@ class App extends Component {
             hasNext: false,
             n: 0,
         };
+        this.handleScroll = this.handleScroll.bind(this);
     }
 
     componentDidMount() {
-        this.update(0);
+        window.addEventListener('scroll', this.handleScroll);
+        window.addEventListener('resize', this.handleScroll);
+
+        // Prevent Chrome from remembering scroll position
+        // See https://stackoverflow.com/a/38270059/1313757
+        if ('scrollRestoration' in window.history) {
+            window.history.scrollRestoration = 'manual';
+        }
+
+        const progress = Number(cookies.get('Progress')) || 0;
+        this.update(progress);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll);
+        window.removeEventListener('resize', this.handleScroll);
+    }
+
+    handleScroll() {
+        const doc = document.documentElement || document.body.parentNode || document.body;
+        const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : doc.scrollTop;
+        console.log(scrollTop);
     }
 
     callApi = async (n) => {
@@ -33,11 +58,19 @@ class App extends Component {
 
     update = (diff) => {
         const { n } = this.state;
-        this.callApi(n + diff)
+        const newN = n + diff;
+
+        this.callApi(newN)
             .then(res => {
                 const { data, title, header, hasPrev, hasNext } = res;
                 const image = (
-                    <img className="strip-img" alt={title} title={title} src={`/images/${data}`} />
+                    <img
+                        className="strip-img"
+                        alt={title}
+                        title={title}
+                        src={`/images/${data}`}
+                        key={newN}
+                    />
                 );
 
                 this.setState(prevState => ({
@@ -45,19 +78,17 @@ class App extends Component {
                     header,
                     hasPrev,
                     hasNext,
-                    n: n + diff,
+                    n: newN,
                 }));
+
+                // cookies.set('Progress', newN, { path: '/' });
             })
             .catch(err => console.log(err));
     }
 
-    next = () => {
-        this.update(1);
-    }
+    next = () => this.update(1);
 
-    prev = () => {
-        this.update(-1);
-    }
+    prev = () => this.update(-1);
 
     render() {
         const { state, prev, next } = this;
